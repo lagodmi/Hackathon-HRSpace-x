c948cf2145c23abddad4505739db7f190afcc130
Nicktsim <n.tsimbalov98@gmail.com>
2024-03-19 19:10:33 +0700

    add urls.py,update serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets, filters
+from rest_framework import permissions
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .serializers import (InquirySerializer)
+from inquiries.models import (Inquiry)
+
+
+class InquiryViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для для заявок.
+    """
+    queryset = Inquiry.objects.all()
+    # pagination_class = CustomPaginator
+    # permission_classes = (IsAuthorStaffOrReadOnly,)
+    # filter_backends = (DjangoFilterBackend, )
+    # filterset_class = RecipeFilter
