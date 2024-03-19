ff32d3d9783238ffaafcae50786288ead324dd24
Dmitrii <lagodnov@gmail.com>
2024-03-19 15:52:24 +0300

    work on the serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -39,66 +39,6 @@ class CompanySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class ConditionsSerializer(serializers.ModelSerializer):
-    """
-        Сериализатор для модели условия работы.
-    """
-
-    class Meta:
-        model = Conditions
-        fields = "__all__"
-
-
-class DescriptionSerializer(serializers.ModelSerializer):
-    """
-        Сериализатор для модели описание вакансии.
-    """
-
-    class Meta:
-        model = Description
-        fields = "__all__"
-
-
-class DutySerializer(serializers.ModelSerializer):
-    """
-        Сериализатор для модели обязанности.
-    """
-
-    class Meta:
-        model = Duty
-        fields = "__all__"
-
-
-class InquirySerializer(serializers.ModelSerializer):
-    """
-        Сериализатор для модели заявки.
-    """
-
-    class Meta:
-        model = Inquiry
-        fields = "__all__"
-
-
-class PartnershipSerializer(serializers.ModelSerializer):
-    """
-        Сериализатор для модели условия сотрудничества.
-    """
-
-    class Meta:
-        model = Partnership
-        fields = "__all__"
-
-
-class ProfessionSerializer(serializers.ModelSerializer):
-    """
-        Сериализатор для модели профессии.
-    """
-
-    class Meta:
-        model = Profession
-        fields = "__all__"
-
-
 class ProfessionAreaSerializer(serializers.ModelSerializer):
     """
         Сериализатор для модели профессиональной области.
@@ -106,26 +46,6 @@ class ProfessionAreaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProfessionArea
-        fields = "__all__"
-
-
-class RecruiterSerializer(serializers.ModelSerializer):
-    """
-        Сериализатор для модели требование к рекрутерам.
-    """
-
-    class Meta:
-        model = Recruiter
-        fields = "__all__"
-
-
-class SkillSerializer(serializers.ModelSerializer):
-    """
-        Сериализатор для модели навыка.
-    """
-
-    class Meta:
-        model = Skill
         fields = "__all__"
 
 
@@ -167,3 +87,92 @@ class TaskRecruiterSerializer(serializers.ModelSerializer):
     class Meta:
         model = TaskRecruiter
         fields = "__all__"
+
+
+class ProfessionSerializer(serializers.ModelSerializer):
+    """
+        Сериализатор для модели профессии.
+    """
+    prof_area = ProfessionAreaSerializer()
+
+    class Meta:
+        model = Profession
+        fields = ('id', 'prof_area', 'prof_name')
+
+
+class DutySerializer(serializers.ModelSerializer):
+    """
+        Сериализатор для модели обязанности.
+    """
+    profession = ProfessionSerializer()
+
+    class Meta:
+        model = Duty
+        fields = ('id', 'name', 'profession')
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    """
+        Сериализатор для модели навыка.
+    """
+    profession = ProfessionSerializer()
+
+    class Meta:
+        model = Skill
+        fields = ('id', 'name', 'profession')
+
+
+
+
+
+
+
+
+# class ConditionsSerializer(serializers.ModelSerializer):
+#     """
+#         Сериализатор для модели условия работы.
+#     """
+
+#     class Meta:
+#         model = Conditions
+#         fields = "__all__"
+
+
+# class DescriptionSerializer(serializers.ModelSerializer):
+#     """
+#         Сериализатор для модели описание вакансии.
+#     """
+
+#     class Meta:
+#         model = Description
+#         fields = "__all__"
+
+
+# class InquirySerializer(serializers.ModelSerializer):
+#     """
+#         Сериализатор для модели заявки.
+#     """
+
+#     class Meta:
+#         model = Inquiry
+#         fields = "__all__"
+
+
+# class PartnershipSerializer(serializers.ModelSerializer):
+#     """
+#         Сериализатор для модели условия сотрудничества.
+#     """
+
+#     class Meta:
+#         model = Partnership
+#         fields = "__all__"
+
+
+# class RecruiterSerializer(serializers.ModelSerializer):
+#     """
+#         Сериализатор для модели требование к рекрутерам.
+#     """
+
+#     class Meta:
+#         model = Recruiter
+#         fields = "__all__"
