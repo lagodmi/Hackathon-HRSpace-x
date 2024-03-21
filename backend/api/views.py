5c8a49ec11478d70af437d987f5bfd67dfad8508
Dmitrii <lagodnov@gmail.com>
2024-03-21 11:29:50 +0300

    fix views

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,13 +3,30 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
-from .serializers import (InquirySerializer)
-from inquiries.models import (Inquiry,Software,Duty)
+from .serializers import (
+    CitySerializer,
+    InquirySerializer,
+    ProfessionAreaSerializer,
+    ProfessionSerializer
+)
+from inquiries.models import (
+    Duty,
+    City,
+    Company,
+    Inquiry,
+    Profession,
+    ProfessionArea,
+    Software,
+    SkillRecruiter,
+    SocialPackage,
+    TaskAdditional,
+    TaskRecruiter,
+)
 
 
 class InquiryViewSet(viewsets.ModelViewSet):
     """
-        Вьюсет для для заявок.
+        Вьюсет для заявок.
     """
     queryset = Inquiry.objects.all()
     serializer_class = InquirySerializer
@@ -17,3 +34,45 @@ class InquiryViewSet(viewsets.ModelViewSet):
     # permission_classes = (IsAuthorStaffOrReadOnly,)
     # filter_backends = (DjangoFilterBackend, )
     # filterset_class = RecipeFilter
+
+    def create(self, request):
+        # Блок профессия.
+        prof_data = request.data.get('prof')
+        prof_area_data = prof_data.pop('prof_area')
+        try:
+            profession_area = ProfessionArea.objects.get(
+                name=prof_area_data
+            )
+            profession = Profession.objects.get(
+                id=prof_data['id'],
+                prof_area=profession_area,
+                prof_name=prof_data['prof_name']
+            )
+        except Profession.DoesNotExist:
+            profession_serializer = ProfessionSerializer(data=prof_data)
+            if profession_serializer.is_valid():
+                profession = profession_serializer.save()
+            else:
+                return Response(profession_serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        # Блок город.
+        city_data = request.data.get('city')
+        try:
+            city = City.objects.get(id=city_data['id'],
+                                    name=city_data['name'])
+        except City.DoesNotExist:
+            city_serializer = CitySerializer(data=city_data)
+            if city_serializer.is_valid():
+                city = city_serializer.save()
+            else:
+                return Response(city_serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+        # Привязка к модели Inquiry
+        Inquiry.objects.create(prof=profession,
+                               city=city)
--- backend/inquiries/management/commands/loaddb.py
+++ backend/inquiries/management/commands/loaddb.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 import json
 
-from inquiries.models import(
+from inquiries.models import (
     Duty,
     Company,
     Profession,
@@ -15,57 +15,54 @@ from inquiries.models import(
 
 
 array = {
-    Company: 'data/companies.json',
-    Software: 'data/programs.json',
-    SkillRecruiter: 'data/specialSkills.json',
-    SocialPackage: 'data/socialPackage.json',
-    TaskAdditional: 'data/additionalTasks.json',
-    TaskRecruiter: 'data/recruiterTasks.json',
+    Company: "data/companies.json",
+    Software: "data/programs.json",
+    SkillRecruiter: "data/specialSkills.json",
+    SocialPackage: "data/socialPackage.json",
+    TaskAdditional: "data/additionalTasks.json",
+    TaskRecruiter: "data/recruiterTasks.json",
 }
 
 
 class Command(BaseCommand):
-    help = 'Populates table from json'
+    help = "Populates table from json"
 
     def handle(self, *args, **options):
         for model, add_file in array.items():
-            with open(add_file, 'r', encoding='utf-8') as file:
+            with open(add_file, "r", encoding="utf-8") as file:
                 reader = json.load(file)
                 for row in reader:
-                    model.objects.create(
-                        name=row['name']
-                    )
-            self.stdout.write(self.style.SUCCESS(
-                f'Data from {add_file} loaded successfully'
-            ))
+                    model.objects.create(name=row["name"])
+            self.stdout.write(
+                self.style.SUCCESS(f"Data from {add_file} loaded successfully")
+            )
 
-        with open('data/prof.json', 'r', encoding='utf-8') as file:
+        with open("data/prof.json", "r", encoding="utf-8") as file:
             reader = json.load(file)
             for row in reader:
                 prof_area, _ = ProfessionArea.objects.get_or_create(
-                    name=row['prof_area']
+                    name=row["prof_area"]
                 )
                 Profession.objects.create(
-                    id=row['id'],
+                    id=row["id"],
                     prof_area=prof_area,
-                    prof_name=row['prof_name']
+                    prof_name=row["prof_name"]
                 )
 
-        self.stdout.write(self.style.SUCCESS(
-            'Data from data/prof.json loaded successfully'
-        ))
+        self.stdout.write(
+            self.style.SUCCESS("Data from data/prof.json loaded successfully")
+        )
 
-        with open('data/skill.json', 'r', encoding='utf-8') as file:
+        with open("data/skill.json", "r", encoding="utf-8") as file:
             reader = json.load(file)
             for row in reader:
                 prof_area, _ = ProfessionArea.objects.get_or_create(
-                    name=row['prof_area']
+                    name=row["prof_area"]
                 )
-                Duty.objects.create(
-                    prof_area=prof_area,
-                    name=row['name']
-                )
+                Duty.objects.create(prof_area=prof_area, name=row["name"])
 
-        self.stdout.write(self.style.SUCCESS(
-            'Data from data/software.json loaded successfully'
-        ))
+        self.stdout.write(
+            self.style.SUCCESS(
+                "Data from data/software.json loaded successfully"
+            )
+        )
