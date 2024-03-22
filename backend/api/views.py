b53ac8970f8ad92c2a7a80ac3e467d2b1faa1ef2
Nicktsim <n.tsimbalov98@gmail.com>
2024-03-22 22:15:44 +0700

    update

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,7 +10,11 @@ from .serializers import (
     ProfessionAreaSerializer,
     ProfessionSerializer,
     ProfessionGetSerializer,
-    InquiryGetSerializer
+    InquiryGetSerializer,
+    DescriptionSerializer,
+    ConditionsSerializer,
+    PartnershipSerializer,
+    RecruiterSerializer,
 )
 from inquiries.models import (
     Duty,
@@ -54,14 +58,14 @@ class InquiryViewSet(viewsets.ModelViewSet):
     # filterset_class = RecipeFilter
 
     def get_serializer_class(self):
-        if self.request.method in permissions.SAFE_METHODS:
+        if self.request.method == 'GET':
             return InquiryGetSerializer
         return InquirySerializer
 
     def create(self, request):
         # Блок профессия.
-        prof_data = request.data.get('prof')
-        prof_area_data = prof_data.pop('prof_area')
+        prof_data = request.data['prof']
+        prof_area_data = prof_data['prof_area']
         try:
             profession_area = ProfessionArea.objects.get(
                 name=prof_area_data
@@ -80,7 +84,7 @@ class InquiryViewSet(viewsets.ModelViewSet):
                                 status=status.HTTP_400_BAD_REQUEST)
 
         # Блок город.
-        city_data = request.data.get('city')
+        city_data = request.data['city']
         try:
             city = City.objects.get(id=city_data['id'],
                                     name=city_data['name'])
@@ -92,10 +96,86 @@ class InquiryViewSet(viewsets.ModelViewSet):
                 return Response(city_serializer.errors,
                                 status=status.HTTP_400_BAD_REQUEST)
 
+        # Описание.
+        desc_data = {
+            'education': request.data['education'],
+            'experience': request.data['experience'],
+            'citizenship': request.data['citizenship'],
+            'drivingLicense': request.data['drivingLicense'],
+            'carOwnership': request.data['carOwnership']
+        }
 
+        desc_serializer = DescriptionSerializer(data=desc_data)
+        if desc_serializer.is_valid():
+            desc = desc_serializer.save()
+        else:
+            return Response(desc_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        # Условия работы.
+        cond_data = {
+            'workSchedule': request.data['workSchedule'],
+            'workFormat': request.data['workFormat'],
+            'contractType': request.data['contractType'],
+            'socialPackage': request.data['socialPackage']
+        }
 
+        cond_serializer = ConditionsSerializer(data=cond_data)
+        if cond_serializer.is_valid():
+            conditions = cond_serializer.save()
+        else:
+            return Response(cond_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Условия сотрудничества.
+        partnership_data = {
+            'employeeReward': request.data['employeeReward'],
+            'paymentType': request.data['paymentType'],
+            'employeeCount': request.data['employeeCount'],
+            'recruiterTasks': request.data['recruiterTasks'],
+            'desiredFirstResumeDate': request.data['desiredFirstResumeDate'],
+            'desiredEmployeeExitDate': request.data['desiredEmployeeExitDate'],
+            'resumeFormat': request.data['resumeFormat']
+        }
+
+        partnership_serializer = PartnershipSerializer(data=partnership_data)
+        if partnership_serializer.is_valid():
+            partnership = partnership_serializer.save()
+        else:
+            return Response(cond_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Требование к рекрутерам.
+        recruiter_data = {
+            'experienceYears': request.data['experienceYears'],
+            'specialSkills': request.data['specialSkills'],
+            'additionalTasks': request.data['additionalTasks'],
+            'isIndividual': request.data['isIndividual'],
+            'blacklistedCompanies': request.data['blacklistedCompanies'],
+            'recruiterCount': request.data['recruiterCount']
+        }
+
+        recruiter_serializer = RecruiterSerializer(data=recruiter_data)
+        if recruiter_serializer.is_valid():
+            recruiter = recruiter_serializer.save()
+        else:
+            return Response(cond_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         # Привязка к модели Inquiry
-        Inquiry.objects.create(prof=profession,
-                               city=city)
+        inquiry_data = {
+            'name': request.data['name'],
+            'prof': profession,
+            'salary_min': request.data['salaryRange']['salary_min'],
+            'salary_max': request.data['salaryRange']['salary_max'],
+            'city': city,
+            'employeeResponsibilities': request.data['employeeResponsibilities'],
+            'description': desc,
+            'conditions': conditions,
+            'partnership': partnership,
+            'recruiter': recruiter
+        }
+
+        inquiry_serializer = InquirySerializer(data=inquiry_data)
+        if inquiry_serializer.is_valid():
+            inquiry = inquiry_serializer.save()
+        else:
+            return Response(inquiry_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(InquirySerializer(inquiry).data)
